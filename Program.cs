00c6f7e6f48244e10c215f6478975dc61fd849f8
Emelianna Axelsson <emelianna.axelsson@gmail.com>
2026-09-14 15:42:41 +0200

    ändringar

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Bankkonto salaryAccount = new("1234567890","Sven Svensson", "Salary Account", 30000, 0, 0, true); 
+
+Bankkonto saveAccount = new("2234567891","Sven Svensson", "Save Account", 3000000, 0.02, 0, true); 
+
+Bankkonto creditAccount = new("3234567880","Sven Svensson", "Credit Account", 300, -0.07, -100000, true); 
---
+++ bankkonto.cs
@@ -0,0 +1,26 @@
+class Bankkonto(string accountNumber, 
+string accountName, string accountType, 
+double balance, double interest, 
+double creditLimit, bool blocked)
+{
+    string AccountNumber = accountNumber;
+    string AccountName = accountName;
+    string AccountType = accountType;
+    double Balance = balance;
+    double Interest = interest;
+    double CreditLimit = creditLimit;
+    bool Blocked = blocked;
+
+
+public double Deposit ()
+{
+  double deposit = 1000;  
+  
+}
+
+
+}
+
+
+
+
