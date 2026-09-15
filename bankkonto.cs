f989f0f690cbc332d045e23c75cd925f32245918
Emelianna Axelsson <emelianna.axelsson@gmail.com>
2026-09-15 12:55:11 +0200

    La till metod Withdraw samt tankar kring fortsatt arbete i kommentarer

--- bankkonto.cs
+++ bankkonto.cs
@@ -1,10 +1,11 @@
-class Bankkonto(string accountNumber, 
+class Bankkonto
+(string accountNumber,                    //parameterlistan är konstruktorn
 string accountName, string accountType, 
 double balance, double interest, 
 double creditLimit, bool blocked)
 {
-    string AccountNumber = accountNumber;
-    string AccountName = accountName;
+    string AccountNumber = accountNumber;     //fält som använder konstruktorn för att ge fälten sina startvärden
+    string AccountName = accountName;         //de är inte public - ingen kod utanför klassen kan komma åt dem
     string AccountType = accountType;
     double Balance = balance;
     double Interest = interest;
@@ -12,14 +13,23 @@ double creditLimit, bool blocked)
     bool Blocked = blocked;
 
 
-public double Deposit ()
+public void Deposit(double deposit)               //Metod för att sätta in pengar.  
+{                                                 //Behöver hindra från att sätta in negativt belopp  
+  deposit = 1000;                                 //Hindra om kontot är spärrat
+                                                  //Ska insättningarna ske mellan dessa tre konton??
+                                                  //Hindra från att frånkonto och -tillkonto är samma
+}
+
+public void Withdraw(double amount)               //Metod för att ta ut pengar. Behöver hindra att ta ut om det är tomt, 
+                                                  //såvida det inte är tillåtet
+                                                  //Behöver också hindra att ta ut om det är spärrat
 {
-  double deposit = 1000;  
-  
+   amount = 1000; 
 }
 
-
 }
+
+//Console.ReadLine för att inhämta summor att sätta in/föra över? 
 
 
 
