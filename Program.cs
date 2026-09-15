c9b1c6d62dae35df2c16024e6ba4cf952a37f8dd
Emelianna Axelsson <emelianna.axelsson@gmail.com>
2026-09-15 15:03:52 +0200

    Metoden Amount och ChoosedAccount mm

--- Program.cs
+++ Program.cs
@@ -1,5 +1,38 @@
-﻿Bankkonto salaryAccount = new("1234567890","Sven Svensson", "Salary Account", 30000, 0, 0, true); 
+﻿using System.Globalization;
+
+Bankkonto salaryAccount = new("1234567890","Sven Svensson", "Salary Account", 30000, 0, 0, true); 
 
 Bankkonto saveAccount = new("2234567891","Sven Svensson", "Save Account", 3000000, 0.02, 0, true); 
 
 Bankkonto creditAccount = new("3234567880","Sven Svensson", "Credit Account", 300, -0.07, -100000, true); 
+
+List<Bankkonto> BankAccounts = [salaryAccount, saveAccount, creditAccount];
+
+
+
+
+static int ChosedAccount(string prompt)
+{
+    Console.WriteLine(prompt);
+    string? input = Console.ReadLine();
+    bool isValid = int.TryParse(input, out int number);
+
+    while (!isValid || number < 1 || number > 3)
+    {
+        if (!isValid)
+        {
+            Console.WriteLine("Det där var inget heltal. Ange 1, 2 eller 3: ");
+        }
+        else
+        {
+            Console.WriteLine("Ange 1, 2 eller 3: ");
+        }
+
+        input = Console.ReadLine();
+        isValid = int.TryParse(input, out number);
+    }
+
+    int index = number - 1;
+    return index;
+}
+
--- bankkonto.cs
+++ bankkonto.cs
@@ -12,11 +12,44 @@ double creditLimit, bool blocked)
     double CreditLimit = creditLimit;
     bool Blocked = blocked;
 
+List<double> MoneyInAccount = [balance];
 
-public void Deposit(double deposit)               //Metod för att sätta in pengar.  
+static double Amount(string prompt) //Metod för att fråga och ta emot summa. Ska användas tillsammans m Deposit och Withdrawl.
+{                               
+    Console.WriteLine(prompt);
+    string? input = Console.ReadLine();
+    bool isValid = int.TryParse(input, out int number);
+
+    while (!isValid || number == 0) //För att säkra att svaret inte är 0 eller annat än ett nummer
+    {
+        if (!isValid)
+        {
+            Console.Write("Det där var inget heltal. Vänligen ange ett heltal: ");
+        }
+        else
+        {
+            Console.Write("Du har angett 0. Vänligen ange ett heltal: ");
+        }
+ 
+        input = Console.ReadLine();                        
+        isValid = int.TryParse(input, out number);          
+}
+        return number;
+    
+}
+
+
+
+
+
+
+public void Deposit(string prompt)               //Metod för att sätta in pengar.  
 {                                                 //Behöver hindra från att sätta in negativt belopp  
-  deposit = 1000;                                 //Hindra om kontot är spärrat
-                                              
+  double deposit = Amount("Hur mycket vill du sätta in? ");                                 //Hindra om kontot är spärrat
+  double depositTo = 
+  
+  
+  MoneyInAccount.Add(deposit);                                            
                                                   //Överföringar mellan dessa tre konton??
                                                   //Hindra från att frånkonto och -tillkonto är samma
 }
@@ -28,6 +61,17 @@ public void Withdraw(double amount)               //Metod för att ta ut pengar.
    amount = 1000; 
 }
 
+
+
+
+
+
+
+public override string ToString() //Gör ett objekt utskrivbart genom att skriva över en metod som redan finns
+                                      //När Console.WriteLine(kemi); körs visas detta nedan gällande kursen kemi   
+    {
+        return $"Konotummer:{AccountNumber} Kontonamn:{AccountName} kontotyp:{AccountType} Saldo:{Balance} Ränta:{Interest} Kreditgräns: {CreditLimit})";
+    }
 }
 
 //Console.ReadLine för att inhämta summor att sätta in/föra över? 
